81745c16516de68fd797c68d659774648d665e5e
Adam Malantonio <adam.malantonio@gmail.com>
2016-03-21 15:04:54 -0400

    shelvingDesignation allows values to not be arrays

--- elements/shelving-designation.js
+++ elements/shelving-designation.js
@@ -2,22 +2,30 @@ var f = require('./factory')
 module.exports = function shelvingDesignation (sd) {
   if (!sd) return ''
 
-  var prefix = (sd.prefix || []).map(function (p) {
+  var prefix = (arrayify(sd.prefix) || []).map(function (p) {
     return f('prefix', {}, p)
   }).join('')
 
-  var itemPart = (sd.itemPart || []).map(function(i) {
+  var itemPart = (arrayify(sd.itemPart) || []).map(function(i) {
     return f('itemPart', {}, i)
   }).join('')
 
-  var suffix = (sd.suffix || []).map(function (s) {
+  var suffix = (arrayify(sd.suffix) || []).map(function (s) {
     return f('suffix', {}, s)
   }).join('')
 
-  return f('shelvingDesignation', {scheme: sd.scheme}, [
+  var scheme = sd.scheme.toUpperCase()
+
+  return f('shelvingDesignation', {scheme: scheme}, [
     prefix,
     (sd.information ? f('information', {}, sd.information) : ''),
     itemPart,
     suffix
   ])
+}
+
+function arrayify (e) {
+  if (!e) return
+
+  return Array.isArray(e) ? e : [e]
 }
